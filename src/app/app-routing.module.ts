7cb09f2b809199d5c05b0fb65fa08c1293a29eba
koderlaci <koder.laci67@gmail.com>
2023-03-06 21:19:25 +0100

    init: project initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/landing", pathMatch: "full"},
+  { path: "landing", component: LandingComponent},
+  { path: "products", component: ProductsComponent},
+  { path: "login", component: LoginComponent},
+  { path: "registration", component: RegistrationComponent},
+  { path: "delivery", component: DeliveryComponent},
+  { path: "cart", component: CartComponent},
+  { path: "admin", component: AdminComponent},
+  { path: "profile", component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    ProductsComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AdminComponent,
+    ProfileComponent,
+    DeliveryComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
