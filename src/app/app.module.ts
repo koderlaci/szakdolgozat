6afe7fd87afc5b88fe8f25672a4f19c28a966d45
koderlaci <koder.laci67@gmail.com>
2023-03-08 19:02:09 +0100

    fix: removed unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -18,7 +17,6 @@ import { CartComponent } from './pages/cart/cart.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     LandingComponent,
     ProductsComponent,
     LoginComponent,
