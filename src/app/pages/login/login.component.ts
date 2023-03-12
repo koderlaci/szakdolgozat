ed19a3cde55b615bb41f285df38c76f1229630a0
koderlaci <koder.laci67@gmail.com>
2023-03-12 14:49:38 +0100

    wip: login component base

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserHandlerService } from 'src/app/services/user-handler.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+
+  constructor(private userHandlerService: UserHandlerService) { }
 
   ngOnInit(): void {
+    this.loginForm.valueChanges.subscribe(form => console.log(form));
+    // this.getAllUsers();
   }
+
+  login() {
+    this.userHandlerService.login(this.loginForm.getRawValue()).subscribe(res => {
+      console.log(res);
+    })
+  }
+
+  /*getAllUsers() {
+    this.userHandlerService.getAllUsers().subscribe(res => {
+      console.log("Response: ");
+      console.log(res);
+    });
+  }*/
 
 }
