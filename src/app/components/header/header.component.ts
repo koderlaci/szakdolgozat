2ececd5fb67089efc4b843a10777f04ebdcf49c6
koderlaci <koder.laci67@gmail.com>
2023-03-16 16:28:21 +0100

    feat: modified navigation for profile and login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserHandlerService } from 'src/app/services/user-handler.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userHandlerService: UserHandlerService) { }
 
   ngOnInit(): void {
   }
-
 }
