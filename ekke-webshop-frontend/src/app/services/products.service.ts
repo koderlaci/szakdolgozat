0245167a59e4b5e8e3f6ae50c24f1cd71d7af8a6
koderlaci <koder.laci67@gmail.com>
2023-09-17 14:10:58 +0200

    feat: productsService extended with new endpoints

--- ekke-webshop-frontend/src/app/services/products.service.ts
+++ ekke-webshop-frontend/src/app/services/products.service.ts
@@ -1,31 +1,49 @@
 import { Injectable, inject } from '@angular/core';
 import { ProductType } from '../types/types';
-import { ProductService } from 'api-generated';
+import { ProductApiService } from 'api-generated';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private productService = inject(ProductService);
+  private productApiService = inject(ProductApiService);
 
   getProducts(type: ProductType) {
     switch (type) {
       case ProductType.MEN:
-        return this.productService.getMenProducts();
+        return this.productApiService.getMenProducts();
 
       case ProductType.WOMEN:
-        return this.productService.getWomenProducts();
+        return this.productApiService.getWomenProducts();
 
       case ProductType.ACCESSARY:
-        return this.productService.getAccessaryProducts();
+        return this.productApiService.getAccessaryProducts();
     }
   }
 
   getAllProductsForProductSlider() {
-    return this.productService.getSliderProducts();
+    return this.productApiService.getSliderProducts();
   }
 
   getProductForProductPageByVariantId(variantId: string) {
-    return this.productService.getProduct(variantId);
+    return this.productApiService.getProduct(variantId);
+  }
+
+  getProductSizesByVariantIdAndColor(variantId: string, color: string) {
+    return this.productApiService.getProductSizesByVariantIdAndColor(
+      variantId,
+      color
+    );
+  }
+
+  getProductColorsByVariantIdAndSize(variantId: string, size: string) {
+    return this.productApiService.getProductColorsByVariantIdAndSize(
+      variantId,
+      size
+    );
+  }
+
+  getProductByDetails(variantId: string, color: string, size: string) {
+    return this.productApiService.getProductByDetails(variantId, color, size);
   }
 }
