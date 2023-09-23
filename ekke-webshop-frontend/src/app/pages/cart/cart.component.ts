6767d94186a06af82db7d4fff7796bd644aca44e
koderlaci <koder.laci67@gmail.com>
2023-09-23 16:57:19 +0200

    wip: cart component payment handling

--- ekke-webshop-frontend/src/app/pages/cart/cart.component.ts
+++ ekke-webshop-frontend/src/app/pages/cart/cart.component.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { PaymentService } from 'src/app/services/payment.service';
+import { UserHandlerService } from 'src/app/services/user-handler.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,16 +10,41 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class CartComponent {
   protected cartService = inject(CartService);
+  protected paymentService = inject(PaymentService);
+  protected userHandlerService = inject(UserHandlerService);
+
+  protected succesfulPayment = signal(false);
 
   protected productsPrice = this.cartService.getCartProductsPrice();
   protected deliveryFee = 100;
   protected totalPrice: number;
 
   constructor() {
+    this.succesfulPayment.set(false);
     this.totalPrice = this.productsPrice + this.deliveryFee;
   }
 
   pay() {
-    console.log('pay');
+    this.paymentService
+      .purchase(this.totalPrice)
+      .then(() => {
+        console.log('sikeres fizetes');
+        this.cartService.clearCart();
+        this.succesfulPayment.set(true);
+      })
+      .catch((e) => {
+        console.log('valami hiba tortent: ' + e);
+      });
+  }
+
+  isPayButtonDisabled(): boolean {
+    if (this.productsPrice === 0) {
+      return true;
+    }
+    if (!this.userHandlerService.userLoggedIn()) {
+      return true;
+    }
+
+    return false;
   }
 }
