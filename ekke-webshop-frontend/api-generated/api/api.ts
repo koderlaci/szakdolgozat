77b991a894fd7e471f945a62a9ab3291a402ca3c
koderlaci <koder.laci67@gmail.com>
2023-10-13 13:22:50 +0200

    chore: api generated

--- ekke-webshop-frontend/api-generated/api/api.ts
+++ ekke-webshop-frontend/api-generated/api/api.ts
@@ -1,7 +1,9 @@
+export * from './cartApi.service';
+import { CartApiService } from './cartApi.service';
 export * from './productApi.service';
 import { ProductApiService } from './productApi.service';
 export * from './shippingAddressApi.service';
 import { ShippingAddressApiService } from './shippingAddressApi.service';
 export * from './userApi.service';
 import { UserApiService } from './userApi.service';
-export const APIS = [ProductApiService, ShippingAddressApiService, UserApiService];
+export const APIS = [CartApiService, ProductApiService, ShippingAddressApiService, UserApiService];
