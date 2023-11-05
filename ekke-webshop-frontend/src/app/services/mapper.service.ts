037aaff1bd11f4313da571e334151964c8c41d50
koderlaci <koder.laci67@gmail.com>
2023-11-05 12:50:03 +0100

    feat: mapper service for maps and translations

---
+++ ekke-webshop-frontend/src/app/services/mapper.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { ProductType } from '../types/types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MapperService {
+  mapStatusName(status: string) {
+    switch (status) {
+      case 'paid':
+        return 'Kifizetve';
+      case 'under preparation':
+        return 'Összekészítés alatt';
+      case 'under delivery':
+        return 'Kiszállítás alatt';
+      case 'ready to collect':
+        return 'Átvételre kész';
+      case 'completed':
+        return 'Teljesített';
+      default:
+        return 'Kifizetve';
+    }
+  }
+
+  mapDeliveryModeName(deliveryMode: string) {
+    switch (deliveryMode) {
+      case 'personal':
+        return 'Személyes';
+      case 'delivery':
+        return 'Házhozszállítás';
+      default:
+        return 'Személyes';
+    }
+  }
+
+  mapProductTypeName(type: ProductType | string) {
+    switch (type) {
+      case ProductType.MEN:
+        return 'Férfi';
+      case ProductType.WOMEN:
+        return 'Női';
+      case ProductType.ACCESSARY:
+        return 'Kiegészítő';
+      case 'men':
+        return 'Férfi';
+      case 'women':
+        return 'Női';
+      case 'accessary':
+        return 'Kiegészítő';
+      default:
+        return 'Férfi';
+    }
+  }
+}
