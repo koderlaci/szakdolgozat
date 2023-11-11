851e2d83770e7524b9f47d79187fca1814bd938e
koderlaci <koder.laci67@gmail.com>
2023-11-11 21:45:37 +0100

    feat: isUserAdmin guard

---
+++ ekke-webshop-frontend/src/app/guards/isUserAdmin/is-user-admin.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserHandlerService } from 'src/app/services/user-handler.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IsUserAdminGuard {
+  private userHandlerService = inject(UserHandlerService);
+  private router = inject(Router);
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.userHandlerService.isUserAdmin()) {
+      return true;
+    } else {
+      this.router.navigate(['/landing']);
+      return false;
+    }
+  }
+}
